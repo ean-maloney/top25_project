2fbb88d123b6f0c40fe94e24b7bedc27ef4c2e00
Ean Maloney <eanmaloney@gmail.com>
2021-09-30 16:52:43 -0400

    Undo

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 #Query code
-username = 'kzblgxklbcxgjk'
-password = '42cbe5c9d571af401f937b7d8b23ff8b49cca031c697b2ad2b222f36fa48dfca'
-host = 'ec2-34-199-209-37.compute-1.amazonaws.com'
-db = 'dd351di0els2o7'
+username = 'godenmyqjrmzoe'
+password = '673e2f643ed4ddbe58c111219261e6872457e45e34e34c617efa029f10429c0f'
+host = 'ec2-54-83-137-206.compute-1.amazonaws.com'
+db = 'dc995n2umb789o'
 
-engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}/{db}')
-
-# @app.route('/', methods=['POST','GET'])
-# def home():
-#     df = pd.read_sql_query(f"SELECT * FROM rankings ORDER_BY(week, rank)")
-#     return render_template('index2.html', tables = [df.to_html(classes='data')], titles = df.columns.values)
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
