985d921a642d9782262f71baecdc38d8fcb6003a
Ean Maloney <eanmaloney@gmail.com>
2021-09-30 16:46:29 -0400

    Adding second page

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ from sqlalchemy import create_engine
 #Initialize
 app = Flask(__name__)
 
+#Query code
+username = 'kzblgxklbcxgjk'
+password = '42cbe5c9d571af401f937b7d8b23ff8b49cca031c697b2ad2b222f36fa48dfca'
+host = 'ec2-34-199-209-37.compute-1.amazonaws.com'
+db = 'dd351di0els2o7'
+
+engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}/{db}')
+
 @app.route('/', methods=['POST','GET'])
 def home():
-    return render_template('index.html')
+    df = pd.read_sql_query(f"SELECT * FROM rankings ORDER_BY(week, rank)")
+    return render_template('index2.html', tables = [df.to_html(classes='data')], titles = df.columns.values)
 
 if __name__ == '__main__':
     app.run()
