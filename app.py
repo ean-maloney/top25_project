7aca32780f53aecf912dce3987e5ca891b3c0f1f
Ean Maloney <eanmaloney@gmail.com>
2021-10-01 10:40:48 -0400

    Trying links

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ db = 'dc995n2umb789o'
 def home():
     return render_template('index.html')
 
+@app.route('/index2.html', methods=['POST','GET'])
+def models():
+    return render_template('index2.html')
+
 if __name__ == '__main__':
     app.run()
